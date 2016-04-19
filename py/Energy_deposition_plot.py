452cbf6a25790116beaf8dad97c510144f82966e
Jonatan Selsing <jselsing@dark-cosmology.dk>
2016-04-19 10:14:12 +0200

    Initial commit

---
+++ py/Energy_deposition_plot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+
+# Plotting
+import matplotlib.pyplot as pl
+import seaborn; seaborn.set_style('ticks')
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
